c7db160b60024b05bfce24fa71f162fb1758aa01
Tyrone <tyronepoulina@hotmail.nl>
2020-02-03 13:42:48 +0100

    Dit is mijn 3de commit

--- index.php
+++ index.php
@@ -5,10 +5,7 @@
     <?php
     // Hier echo ik mijn welkoms tekst 
     echo "<h1>Hoi, welkom op mijn website!</h1>";
-    
-    
-    
-    
     ?>
+    <img src="assets/img/logo.png"/>
 </body>
 </html>
