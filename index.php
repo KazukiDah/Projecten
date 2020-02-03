3f7a29bbf9719e036be9339b72ebc87a8823df82
Tyrone <tyronepoulina@hotmail.nl>
2020-02-03 13:27:19 +0100

    Dit is mijn 2de commit

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-        phpinfo();
+<html>
+<head>
+</head>
+<body>
+    <?php
+    // Hier echo ik mijn welkoms tekst 
+    echo "<h1>Hoi, welkom op mijn website!</h1>";
+    
+    
+    
+    
     ?>
+</body>
+</html>
