7977c6d04c5dbb171fa901d58809b6d875982e82
Tyrone <tyronepoulina@hotmail.nl>
2020-02-10 10:59:46 +0100

    nieuwe echt toegevoegd

--- index.php
+++ index.php
@@ -4,8 +4,24 @@
 <body>
     <?php
     // Hier echo ik mijn welkoms tekst 
-    echo "<h1>Hoi, welkom op mijn website!</h1>";
+    echo "<h1>Hoi, welkom op mijn website</h1>";
     ?>
+
+    <?php 
+    $naam = 'Tyrone ';
+    $leeftijd = '18 ';
+    $lengte = '1.81 ';
+    echo $naam;
+    echo $leeftijd;
+    echo $lengte;
+    ?>
+
+    <?php 
+    echo "Ik ben ".$naam."ik ben ".$leeftijd.'jaar '."oud en ben ".$lengte.'m lang ';
+    
+    ?>
+
     <img src="assets/img/logo.png"/>
+
 </body>
 </html>
