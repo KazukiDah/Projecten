3b8a4fd5509a22ee652ce159509bc320b118bb68
Tyrone <tyronepoulina@hotmail.nl>
2020-02-10 13:33:26 +0100

    Nieuwe echo met Spintf en een nieuwe array!

--- index.php
+++ index.php
@@ -7,9 +7,22 @@
     echo "<h1>Hoi, welkom op mijn website</h1>";
     ?>
 
-    <?php 
-    $mijn_array = array('Tyrone', '18', '1.82');
-    var_dump($mijn_array);
+    <?php
+    $mijn_array = array('Tyrone','18' , '1.82')
+    var_dump($mijn_array) 
+    $naam = 'Tyrone ';
+    $leeftijd = '18 ';
+    $leeftijd2 = (string) $leefttijd;
+    $lengte = '1.81 ';
+    $woonplaats = 'Leiden';
+    
+    echo gettype($leeftijd);
+    $leeftijd2 =(string) $leeftijd;
+
+    echo gettype($leeftijd);
+    echo
+
+
     ?>
 
     <?php 
